a2f4036b7395930a520e5acf762ec378751fc078
9291Sam <sam.skinr@gmail.com>
2023-02-13 10:44:26 -0500

    updated submodules

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+TODO:
+static mesh abstraction:
--- src/world/world.cpp
+++ src/world/world.cpp
@@ -12,6 +12,11 @@ namespace world
         this->objects.push_back(renderer.createObject(std::move(b), std::move(j)));
         this->objects.at(1).transform.scale = {100.0f, 100.0f, 100.0f};
         this->objects.at(1).transform.translation.y -= 10.0f;
+
+        auto [k, l] = render::Object::readVerticesFromFile("../models/cube.obj");
+        this->objects.push_back(renderer.createObject(std::move(k), std::move(l)));
+        this->objects.at(2).transform.scale = {15.0f, 15.0f, 15.0f};
+        this->objects.at(2).transform.translation.y += 50.0f;
     }
 
     const std::vector<render::Object>& World::getObjects() const
